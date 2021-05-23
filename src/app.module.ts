a7d1fdc4d3c5333c041e7579ed45eab859f10fca
Juan Jose Vega <juanvega.tech@gmail.com>
2021-05-23 08:45:35 -0500

    add ValidationPipe, whitelist and forbidNonWhitelisted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from './controllers/products.controller';
+import { CategoriesController } from './controllers/categories.controller';
+import { ProductsService } from './services/products.service';
+import { CategoriesService } from './services/categories.service';
+import { BrandsController } from './controllers/brands.controller';
+import { BrandsService } from './services/brands.service';
+import { UsersController } from './controllers/users.controller';
+import { UsersService } from './services/users.service';
+import { CustomersController } from './controllers/customers.controller';
+import { CustomersService } from './services/customers.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    ProductsController,
+    CategoriesController,
+    BrandsController,
+    UsersController,
+    CustomersController,
+  ],
+  providers: [
+    AppService,
+    ProductsService,
+    CategoriesService,
+    BrandsService,
+    UsersService,
+    CustomersService,
+  ],
 })
 export class AppModule {}
