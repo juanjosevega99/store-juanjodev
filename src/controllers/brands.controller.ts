b459969cafa4b821693a3aa849a3d2607c58029d
Juan Jose Vega <juanvega.tech@gmail.com>
2021-05-22 22:32:00 -0500

    feature: controller and service of brand

---
+++ src/controllers/brands.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+
+import { BrandsService } from '../services/brands.service';
+import { CreateBrandDto, UpdateBrandDto } from '../dtos/brand.dtos';
+
+@Controller('brands')
+export class BrandsController {
+  constructor(private brandsService: BrandsService) {}
+
+  @Get()
+  findAll() {
+    return this.brandsService.findAll();
+  }
+
+  @Get(':id')
+  get(@Param('id', ParseIntPipe) id: number) {
+    return this.brandsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() payload: CreateBrandDto) {
+    return this.brandsService.create(payload);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateBrandDto,
+  ) {
+    return this.brandsService.update(id, payload);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.brandsService.remove(+id);
+  }
+}
