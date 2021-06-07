a8dd0e58a83af750afebb26bba3eabf87ec5307c
Juan Jose Vega <juanvega.tech@gmail.com>
2021-06-06 20:38:23 -0500

    database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-import { ConfigModule } from '@nestjs/config';
-
 import { environments } from './environments';
+import { DatabaseModule } from './database/database.module';
+import config from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
     }),
     UsersModule,
     ProductsModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
