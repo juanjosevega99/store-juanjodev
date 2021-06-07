7a9f1a2880e0348a120dc51cb10643b4d5ce98fa
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-06-06 20:57:16 -0500

    mongoose conection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,11 +1,27 @@
 import { Module, Global } from '@nestjs/common';
 import { MongoClient } from 'mongodb';
 import { ConfigType } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import config from '../config';
 
 @Global()
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { connection, user, password, host, port, dbName } =
+          configService.mongo;
+        return {
+          uri: `${connection}://${host}:${port}`,
+          user,
+          pass: password,
+          dbName,
+        };
+      },
+      inject: [config.KEY],
+    }),
+  ],
   providers: [
     {
       provide: 'MONGO',
@@ -13,7 +29,7 @@ import config from '../config';
         const { connection, user, password, host, port, dbName } =
           configService.mongo;
         const uri = `${connection}://${user}:${password}@${host}:${port}/?authSource=admin&readPreference=primary`;
-        const client = new MongoClient(uri);
+        const client = new MongoClient(uri, { useUnifiedTopology: true });
         await client.connect();
         const database = client.db(dbName);
         return database;
@@ -21,6 +37,6 @@ import config from '../config';
       inject: [config.KEY],
     },
   ],
-  exports: ['MONGO'],
+  exports: ['MONGO', MongooseModule],
 })
 export class DatabaseModule {}
